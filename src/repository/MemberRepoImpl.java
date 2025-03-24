a6c0c4f867d8f3310256fc13eed419623790543d
kya <dbsdk4211@naver.com>
2025-03-24 17:49:34 +0900

    fix : 결과값 오류 수정 db변동 있었으나 변동결과를 가져오지 못함 프로시저는 변동 갯수를 가져올 수 없으므로 true false 값으로 가져와 적용

--- src/repository/MemberRepoImpl.java
+++ src/repository/MemberRepoImpl.java
@@ -260,9 +260,8 @@ public class MemberRepoImpl implements MemberRepo {
             cs = conn.prepareCall(sql);
             cs.setString(1,memberId);
 
-            int rs = cs.executeUpdate();
-
-            if(rs>0) return Optional.of(memberId);
+            boolean flag = cs.execute();
+            if(!flag) return Optional.of(memberId);
             else return Optional.empty();
         } catch (SQLException e) {
             e.printStackTrace();
