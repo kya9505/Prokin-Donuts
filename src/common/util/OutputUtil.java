0d24333b8044258d7a10362562e242e1f1206cbb
kya <dbsdk4211@naver.com>
2025-03-30 17:47:48 +0900

    refactor : output util - OutputUtil을 만들어 출력 기능 분리 - 제네릭 T를 이용해 모든 파트에서 사용가능하도록 제작

--- src/common/util/OutputUtil.java
+++ src/common/util/OutputUtil.java
@@ -6,4 +6,13 @@ import java.util.List;
 import java.util.Optional;
 
 public class OutputUtil {
+
+    public static  <T> void output(T result){
+        System.out.println(result);
+    }
+
+
+    public static <T> void outputList(List<T> result){
+        result.forEach(System.out::println);
+    }
 }
